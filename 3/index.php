ebec9b7099bb48a9136abd9193a02eaa261270af
Sadra <123418962+SadraGt@users.noreply.github.com>
2023-02-24 23:54:30 +0330

    Add files via upload

---
+++ 3/index.php
@@ -0,0 +1,14 @@
+<?php
+
+// define('AppName', 'Foxblog');
+
+if(true){
+    define('AppName', 'Foxblog');
+    // const AppVersion = 'v1.0';❌
+}
+const AppVersion = 'v1.0';
+
+echo AppName, '<br>', AppVersion;
+
+
+?>
